7804fc241623d2c485c9fbd090ea39b4a3313ba2
Toktorov <ktoktorov2005@gmail.com>
2023-02-14 22:06:39 +0600

    changes in admin panel

--- apps/users/views.py
+++ apps/users/views.py
@@ -27,9 +27,23 @@ def admin_panel(request):
     setting = Setting.objects.latest('id')
     rooms = Room.objects.all()
     reservations = Reservation.objects.all().order_by('-created')
+    accept_reservations = Reservation.objects.filter(checked = True)
+    refusal_reservations = Reservation.objects.filter(checked = False)
+    if request.method == "POST":
+        if 'accept' in request.POST:
+            reservation_id = request.POST.get('reservation_id')
+            reservation_checked = Reservation.objects.get(id = int(reservation_id))
+            reservation_checked.checked = True
+            reservation_checked.save()
+        if 'delete' in request.POST:
+            reservation_id = request.POST.get('reservation_id')
+            reservation_delete = Reservation.objects.get(id = int(reservation_id))
+            reservation_delete.delete()
     context = {
         'setting' : setting,
         'rooms' : rooms,
-        'reservations' : reservations
+        'reservations' : reservations,
+        'accept_reservations' : accept_reservations,
+        'refusal_reservations' : refusal_reservations,
     }
     return render(request, 'custom_admin/index.html', context)
